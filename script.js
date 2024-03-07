3a78b291bfdc9bd1b33d0404156efb4d8be17c46
Kileia R <139944472+Kileia@users.noreply.github.com>
2024-03-06 16:54:04 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// function toggleOverlay() {
+//     var overlay = document.getElementsByClassName("overlay-container") [0];
+//     var displaySetting = overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
+
+// }
